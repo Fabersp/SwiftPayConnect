b02b2d8f735cb7cfe22dd4986aa6992442ff2a01
Fabricio Padua <fabricio_0505_@hotmail.com>
2025-06-09 10:42:30 -0400

    push start project

---
+++ SwiftPayConnect/SwiftPayConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPayConnectApp.swift
+//  SwiftPayConnect
+//
+//  Created by Fabricio Padua on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPayConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
